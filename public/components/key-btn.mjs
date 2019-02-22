8c5be294ff25c9a0cb1dbcf76aa04da507cd840d
Edgar Valdez <edgarvaldez99@gmail.com>
2019-02-21 21:41:32 -0300

    refactor: chage variable name orange by primary-color

--- public/components/key-btn.mjs
+++ public/components/key-btn.mjs
@@ -1,17 +1,17 @@
 const template = self => `
   <style>
     :host {
-      --orange: #e67e22;
+      --primary-color: #e67e22;
       --space: 1em;
     }
     .key-btn {
-      border: 2px dashed var(--orange);
+      border: 2px dashed var(--primary-color);
       display: inline-block;
       padding: 5px;
       text-align: center;
     }
     .btn {
-      background-color: var(--orange);
+      background-color: var(--primary-color);
       border: 0;
       border-radius: 5px;
       color: white;
@@ -47,16 +47,8 @@ export default class KeyBtn extends HTMLElement {
     this.setAttribute('size', size)
   }
 
-  static get observedAttributes () {
-    return [ 'key', 'size' ];
-  }
-
   connectedCallback () {
     let shadowRoot = this.attachShadow({ mode: 'open' })
     shadowRoot.innerHTML = template(this)
-  }
-
-  attributeChangedCallback (name, oldValue, newValue) {
-    console.log('Attributes changed.', { self: this, name, oldValue, newValue });
   }
 }
